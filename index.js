e376d48f0eaa73bd593a6eca00b424a4e763a4a8
nahmanbo <nhmanbo2003@gmail.com>
2025-07-08 13:12:14 +0300

    add get greet

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from "express";
+import { Timestamp } from "firebase-admin/firestore";
 
 const PORT = 1230;
 
 const server = express();
+server.use(express.json());
+
+server.get('/greet', (req, res) =>{
+    const now = new Date();
+    const formattedDate = now.toLocaleDateString('en-IL'); 
+    res.send({ msg: `hi from get endpoint <${formattedDate}>` });
+  })
+//aa
+
 
 server.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`)
