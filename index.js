c58765570cd24d4e42626071aa92de35b5b6b8f3
nahmanbo <nhmanbo2003@gmail.com>
2025-07-08 16:00:26 +0300

    finish Part 2: Route Params &amp; Internal Requests

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 import express from "express";
-import { Timestamp } from "firebase-admin/firestore";
 
 const PORT = 1230;
-
 const server = express();
+
+//-------------Middleware---------------
+function logger(req, res, next) {
+  console.log(`Got request: method = ${req.method}, url = ${req.url}`);
+  next();
+}
+
+function nameLogger(req, res, next) {
+  console.log(`Received name: ${req.params.name}`);
+  next();
+}
+
 server.use(express.json());
+server.use(logger);
+server.use('/greet/:name', nameLogger);
 
-server.get('/greet', (req, res) =>{
-    const now = new Date();
-    const formattedDate = now.toLocaleDateString('en-IL'); 
-    res.send({ msg: `hi from get endpoint <${formattedDate}>` });
-  })
-//aa
+//-------------Routes---------------
+server.get('/greet', (req, res) => {
+  const now = new Date();
+  const formattedDate = now.toLocaleDateString('en-IL');
+  res.send({ msg: `hi from get endpoint <${formattedDate}>` });
+});
 
+server.get('/greet/:name', (req, res) => {
+  res.send({ msg: `got name <${req.params.name}>` });
+});
 
+server.get(`/test`, (req, res) => {
+  fetch('http://localhost:1230/greet/nahman')
+    .then(response => {
+      if (response.msg === "got name <nahman>")
+        res.send({ result: "Ok" });
+      else
+        res.send({ result: "fail" });
+    });
+});
+
+//-------------Start server---------------
 server.listen(PORT, () => {
-    console.log(`server listening on port ${PORT}`)
-  })
+  console.log(`Server listening on port ${PORT}`);
+});
